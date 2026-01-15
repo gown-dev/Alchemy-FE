9135e38162c4d5662958124e886249bc69075ffe
William Crawford <wccrawford@gmail.com>
2026-01-15 16:16:15 -0500

    Add empty genes and clothing pages to admin.

---
+++ src/app/admin/genes/genes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-genes',
+  imports: [],
+  templateUrl: './genes.component.html',
+  styleUrl: './genes.component.css'
+})
+export class AdminGenesComponent {
+
+}
