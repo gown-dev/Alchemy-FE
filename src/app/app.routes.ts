9631e404b052dfc4cacf7884107935d37e15f345
nudemfisli <nudemfisli@gmail.com>
2026-01-23 17:44:36 +0100

    Update dashboard layout and Alchemy card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,24 @@ import { AdminClothesComponent } from './admin/clothes/clothes.component';
 import { AdminGenesComponent } from './admin/genes/genes.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // redirect empty path to /home
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // Lazy-load standalone HomeComponent
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/homepage/homepage.component').then(
+        (m) => m.HomeComponent
+      ),
+  },
+
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+
   { path: 'admin', component: AdminComponent, canActivate: [AdminGuard] },
   { path: 'admin/clothing', component: AdminClothesComponent, canActivate: [AdminGuard] },
   { path: 'admin/genes', component: AdminGenesComponent, canActivate: [AdminGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'home' },
 ];
